1614fcf1caf209cbf9f121ab85d2f89aecf7bb20
elijahmm17 <elijahmm17@gmail.com>
2019-06-12 18:30:30 +0000

    Required nokogiri and pry in gemspec

--- lib/shelter_pets.rb
+++ lib/shelter_pets.rb
@@ -1,7 +1,5 @@
+require "pry"
+require "nokogiri"
+
 require "./lib/shelter_pets/version"
 require "./lib/shelter_pets/cli"
-
-module ShelterPets
-  class Error < StandardError; end
-  # Your code goes here...
-end
