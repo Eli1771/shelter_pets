445fd9d9fa802fa90348e323e392571f8fc11413
github username <github email address>
2019-06-16 21:15:50 -0700

    Tweaked meain loop in scraper class

--- lib/shelter_pets/dog.rb
+++ lib/shelter_pets/dog.rb
@@ -1,29 +1,26 @@
-class ShelterPets::Dog 
-  attr_accessor :name, :age, :sex, :species, :url 
-  
+class ShelterPets::Dog
+  attr_accessor :name, :age, :sex, :species, :url
+
   @@all = []
-  
+
   def initialize(attr_hash = {})
     attr_hash.each do |attribute, value|
       self.send("#{attribute}=", value)
-    end 
+    end
     @@all << self
-  end 
-  
+  end
+
   def self.top_three
     @@all[0,3]
-  end 
-  
+  end
+
   def additional_info
     puts "Location: Helping Paws Rescue"
     puts "Bio: She's a good girl!"
     puts "Contact: 555-5555"
-  end 
-end 
+  end
+end
 
-ShelterPets::Dog.new({:name => "Boomer", :age => "Puppy", :sex => "male", :url => ""})
-ShelterPets::Dog.new({:name => "Lilly", :age => "Puppy", :sex => "female", :url => ""})
-ShelterPets::Dog.new({:name => "Rex", :age => "Adult", :sex => "male", :url => ""})
-
-
-
+#ShelterPets::Dog.new({:name => "Boomer", :age => "Puppy", :sex => "male", :url => ""})
+#ShelterPets::Dog.new({:name => "Lilly", :age => "Puppy", :sex => "female", :url => ""})
+#ShelterPets::Dog.new({:name => "Rex", :age => "Adult", :sex => "male", :url => ""})
--- lib/shelter_pets/scraper.rb
+++ lib/shelter_pets/scraper.rb
@@ -10,14 +10,13 @@ class ShelterPets::Scraper
     browser.goto "https://theshelterpetproject.org/pet-search/?zip=89120&radius=25&species=dog&resultPage=1"
     sleep 2
     doc = Nokogiri::HTML(browser.html)
-    binding.pry
-    dogs = doc.css('li.search__pet').first
+    dogs = doc.css('div.search__pet-basic-details')
     dogs.each do |dog_info|
       name = dog_info.css('span.search__pet-name').text
       breed = dog_info.css('span.search__pet-breed').text
       sex = dog_info.css('span.search__pet-meta').text.split(" ").first
       age = dog_info.css('span.search__pet-meta').text.split(" ").last
-      attributs = {name: name, breed: breed, sex: sex, age: age}
+      attributes = {name: name, breed: breed, sex: sex, age: age}
       dog = ShelterPets::Dog.new(attributes)
     end
   end
