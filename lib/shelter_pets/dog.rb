553a1ee4c93fb77efb6a25ee757cb30e704c826a
github username <github email address>
2019-07-03 12:41:29 -0700

    Minor tweaks to dog and scraper after assessment

--- lib/shelter_pets/dog.rb
+++ lib/shelter_pets/dog.rb
@@ -14,10 +14,23 @@ class ShelterPets::Dog
     @@all
   end
   def self.clear
-    @@all.clear
+    all.clear
   end
   def self.top_ten
-    @@all[0,10]
+    all[0,10]
+  end
+
+  # add a class method that takes a string argument represetning a dog's name and returns all the dogs matching that name
+
+  def self.find_by_name(name)
+    # matching = []
+    # all.each do |dog|
+    #   if dog.name == name
+    #     matching << dog
+    #   end
+    # end
+    # matching
+    all.select {|dog| dog.name == name}
   end
 
 end
